7a417fffd548328f01c0093dfed07e7321d3caac
Triin Järv <trjarv@tktk.ee>
2021-09-17 23:45:09 +0300

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis
+            //"Oled sündidnud aastal {yearOfBirth}"
+
+            Console.WriteLine("Palun, sisesta oma vanus:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            Console.WriteLine($"Oled sündinud aastal {userAge}.");
+
+        }
+    }
+}
